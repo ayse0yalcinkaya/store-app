344733d3fbc1226febce82845d0962af1a5cddb9
ayse0yalcinkaya <ayse0yalcinkaya@gmail.com>
2024-02-22 16:25:52 +0300

    ilk commit

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+// import products from "../../productData";
+import ProductItem from "./ProductItem";
+import "./Products.css";
+import { useCallback } from "react";
+import FormInputs from "../Form/FormInputs";
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+
+
+  const productList = products.map((product) => (
+    <ProductItem key={product.id} product={product} />
+  ));
+
+
+  const fetchProductsHandler = useCallback( async function()  {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://my-pos-application-api.onrender.com/api/products/get-all"
+      );
+      if (response.status !== 200) {
+        throw new Error("Something went wrong!");
+      }
+      console.log(response);
+      const data = await response.json();
+
+      const newData = data.map((item) => {
+        return {
+          id: item._id,
+          name: item.title,
+          ...item,
+        };
+      });
+      setProducts(newData);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => { //side effect - dışarıdan yapılan istekler, burada API'dan veri çekilmesi olayı useEffect kullanılır 
+    fetchProductsHandler();
+  }, [fetchProductsHandler]);
+
+  let content = <p>Found not products!</p>
+  if(products.length > 0) {
+    content = productList
+  }
+  if(error) {
+    content = <p>{error}</p>
+  }
+  if(isLoading) {
+    content = <p>Loading...</p>
+  }
+
+  return (
+    <main className="products-wrapper">
+      <FormInputs />
+      <ul className="products">{content}</ul>
+      <button className="button" onClick={fetchProductsHandler}>
+        Fetch Products
+      </button>
+    </main>
+   
+    
+  );
+};
+
+export default Products;
